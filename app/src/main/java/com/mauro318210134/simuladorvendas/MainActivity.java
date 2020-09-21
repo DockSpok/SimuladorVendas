ae4199f80f6c15eac5ea96e4b15ed90aaebe4d38
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-09-21 01:54:36 -0300

    MainActivity com evento para chamar VersaoActivity
    Na MainActivity, Implemente o evento de onClick para a variável cliqueBotaoVersao.
    Chamaremos a VersaoActivity utilizando o código de requisição 1000.

--- app/src/main/java/com/mauro318210134/simuladorvendas/MainActivity.java
+++ app/src/main/java/com/mauro318210134/simuladorvendas/MainActivity.java
@@ -2,7 +2,9 @@ package com.mauro318210134.simuladorvendas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,4 +23,12 @@ public class MainActivity extends AppCompatActivity {
         lblTotalAcessorio = findViewById(R.id.lblTotalAcessorio);
         lblTotalGeral = findViewById(R.id.lblTotalGeral);
     }
+
+    View.OnClickListener cliqueBotaoVersao = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Intent intent = new Intent(MainActivity.this, VersaoActivity.class);
+            startActivityForResult(intent, 1000);
+        }
+    };
 }
