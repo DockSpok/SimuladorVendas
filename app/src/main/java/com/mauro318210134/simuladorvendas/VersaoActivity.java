15ae422de7c03a32c91fdef90e82ff54e6dd243c
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-09-21 01:16:08 -0300

    Aruivos inseridos pela ide

---
+++ app/src/main/java/com/mauro318210134/simuladorvendas/VersaoActivity.java
@@ -0,0 +1,14 @@
+package com.mauro318210134.simuladorvendas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VersaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_versao);
+    }
+}
