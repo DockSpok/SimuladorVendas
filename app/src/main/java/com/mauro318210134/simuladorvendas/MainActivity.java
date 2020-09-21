86ca9517753c0a0a01e2d6a97241603b8e6a57d7
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-09-21 01:11:29 -0300

    instanciados os elementos de tela na viewMain

--- app/src/main/java/com/mauro318210134/simuladorvendas/MainActivity.java
+++ app/src/main/java/com/mauro318210134/simuladorvendas/MainActivity.java
@@ -3,12 +3,22 @@ package com.mauro318210134.simuladorvendas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView lblVersao, lblVersaoSelecionada, lblTotalVersao, lblAcessorio, lblAcessorioSelecionado, lblTotalAcessorio, lblTotalGeral;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lblVersao = findViewById(R.id.lblVersao);
+        lblVersaoSelecionada = findViewById(R.id.lblVersaoSelecionada);
+        lblTotalVersao = findViewById(R.id.lblTotalVersao);
+        lblAcessorio = findViewById(R.id.lblAcessorio);
+        lblAcessorioSelecionado = findViewById(R.id.lblAcessorioSelecionado);
+        lblTotalAcessorio = findViewById(R.id.lblTotalAcessorio);
+        lblTotalGeral = findViewById(R.id.lblTotalGeral);
     }
 }
